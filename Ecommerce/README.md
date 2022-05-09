0a85781865fe474fba61b283bd40562e4e6661fd
Evelyn Avilés <96357081+3v3AH@users.noreply.github.com>
2022-05-09 14:17:10 -0500

    Add files via upload

---
+++ Ecommerce/README.md
@@ -0,0 +1,54 @@
+# ProShop E-Commerce MERN Full Stack App
+
+Visit the App fully working!!
+
+link: https://ecommerce-lp1.herokuapp.com/
+
+Create your own account and try out its functionalites as an user.
+
+Or log in as an admin with the following account:
+
+email: admin@example.com <br>
+password: 123456
+
+... and see the functionalites you can have as an admin too!
+
+Get to know more about my story on: www.lucianopastine.tech
+
+<br>
+
+# ProShop E-Commerce MERN Full Stack App [PT-BR]
+
+Dê uma olhada no site em pleno funcionamento!!
+
+link: https://ecommerce-lp1.herokuapp.com/
+
+Crie a sua própria conta como usuário e experimente as funcionalidades que o site tem.
+
+Ou faça login como administrador na seguinte conta:
+
+email: admin@example.com <br>
+password: 123456
+
+... e veja as funcionalidades para administradores também!
+
+Conheça um pouco mais da minha história em: www.lucianopastine.tech
+
+<br>
+
+# ProShop E-Commerce MERN Full Stack App [ES]
+
+Mire cómo funciona el site en pleno funcionamento!!
+
+link: https://ecommerce-lp1.herokuapp.com/
+
+Crie su propia cuenta como usuario y pruebe las funcionalidades que tiene el site.
+
+O haga login como administrador con la cuenta siguiente:
+
+email: admin@example.com <br>
+password: 123456
+
+... y vea las funcionalidade que el site tiene para administradores también!
+
+Conózca un poco más de mi historia en: www.lucianopastine.tech
---
+++ Ecommerce/frontend/src/store.js
@@ -0,0 +1,78 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { 
+    productListReducer, 
+    productDetailsReducer,
+    productDeleteReducer,
+    productCreateReducer,
+    productUpdateReducer,
+    productCreateReviewReducer,
+    productTopRatedReducer
+} from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
+import { 
+    userLoginReducer, 
+    userRegisterReducer, 
+    userDetailsReducer, 
+    userUpdateProfileReducer,
+    userListReducer,
+    userDeleteReducer,
+    userUpdateReducer
+} from './reducers/userReducers'
+import { 
+    orderCreateReducer, 
+    orderDetailsReducer, 
+    orderPayReducer, 
+    orderDeliverReducer, 
+    myOrdersReducer,
+    orderListReducer
+} from './reducers/orderReducers'
+
+const reducer = combineReducers({
+    // Product Reducers
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+    productDelete: productDeleteReducer,
+    productCreate: productCreateReducer,
+    productUpdate: productUpdateReducer,
+    productCreateReview: productCreateReviewReducer,
+    productTopRated: productTopRatedReducer,
+    // Cart Reducers
+    cart: cartReducer,
+    // User Reducers
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
+    userList: userListReducer,
+    userDelete: userDeleteReducer,
+    userUpdate: userUpdateReducer,
+    // Order Reducers
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
+    orderDeliver: orderDeliverReducer,
+    myOrders: myOrdersReducer,
+    orderList: orderListReducer
+})
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
+const initialState = {
+    cart: { 
+        cartItems: cartItemsFromStorage, 
+        shippingAddress: shippingAddressFromStorage 
+    },
+    userLogin: { userInfo: userInfoFromStorage }
+}
+
+const middleware = [thunk]
+
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store
